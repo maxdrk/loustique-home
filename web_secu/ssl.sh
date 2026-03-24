c54835aab24a1d0090f5834c1fb25da804edd7b6
maxime <maxime.derick1235478@gmail.com>
2026-03-25 00:38:26 +0100

    1.3

--- web_secu/ssl.sh
+++ web_secu/ssl.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-
-mkdir ssl
-chown ${SUDO_USER}:${SUDO_USER} ssl/
-
-if [ -f "web_secu/ssl/cert.pem" ] && [ -f "web_secu/ssl/key.pem" ]; then
-    echo "Certificat SSL déjà existant, on passe."
+if [ -d "web_secu/ssl" ]; then
+    echo "Le dossier ssl existe déjà."
+    chown ${SUDO_USER}:${SUDO_USER} web_secu/ssl/
 else
     mkdir -p web_secu/ssl
+    chown ${SUDO_USER}:${SUDO_USER} web_secu/ssl/
+    echo "Dossier ssl créé."
+fi
+
+if [ -f "web_secu/ssl/cert.pem" ] && [ -f "web_secu/ssl/key.pem" ]; then
+    echo "Certificat SSL déjà existant"
+else
     openssl req -x509 -newkey rsa:4096 -keyout web_secu/ssl/key.pem -out web_secu/ssl/cert.pem -days 365 -nodes \
         -subj "/C=BE/ST=Brabant Wallon/L=Louvain-La-Neuve/O=Les Loustiques/OU=EPHEC/CN=loustiques.local"
     echo "Certificat généré avec succès !"
