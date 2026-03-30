488289e2cf32061fc0e7808702285b54978a9955
maxime <maxime.derick1235478@gmail.com>
2026-03-30 23:21:28 +0200

    .

--- fastapi/main.py
+++ fastapi/main.py
@@ -10,17 +10,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 composants = os.path.join(BASE_DIR, "composants", "byPanda")
 sys.path.insert(0, composants) 
 
-from lumieres import SystemeLumieres
+#from lumieres import SystemeLumieres
 from thermostat import SystemeThermostat
-from volets import SystemeVolets
-from septsegments import afficher_temperature
+#from volets import SystemeVolets
+from septsegments import afficher_temperature # <-- CORRIGÉ : On a enlevé le "#" !
 
 app = FastAPI(title="Loustiques API - Pi 2")
 
-controleur_lumieres = SystemeLumieres()
+#controleur_lumieres = SystemeLumieres()
 controleur_thermostat = SystemeThermostat()
 #controleur_volet = SystemeVolets()
-
+"""
 @app.get("/up_led")
 async def allumer_led():
     controleur_lumieres.allumerLumieres()
@@ -32,14 +32,19 @@ async def eteindre_led():
     controleur_lumieres.eteindreLumieres()
     controleur_lumieres.modeManuel = True 
     return {"success": True, "message": "Lumière éteinte par le Pi 2"}
-
+"""
 @app.get("/temperature")
 async def read_temp():
     temp = controleur_thermostat.lireTemperature()
+    
+    # 1. Si la lecture échoue, on renvoie une erreur et on arrête là
     if temp is None:
         return {"success": False, "message": "Impossible de lire le capteur DHT11"}
+        
+    # 2. CORRIGÉ : On affiche la température sur l'écran ICI (en dehors du if)
     afficher_temperature(temp)
-    
+        
+    # 3. On renvoie le succès au site web
     return {"success": True, "temperature": temp}
 
 
