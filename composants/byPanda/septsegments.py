6d8f7dad4ab71261532c85e90597338364c090c2
maxime <maxime.derick1235478@gmail.com>
2026-03-31 20:36:38 +0200

    Maj 7segment

--- composants/byPanda/septsegments.py
+++ composants/byPanda/septsegments.py
@@ -1,18 +1,17 @@
 import tm1637
 import time as t
 
-
 display = tm1637.TM1637(clk=4, dio=17)
 display.brightness(2)
 
-def afficher_temperature(temperature,temperature_moyenne):
-    print(f"Test affichage: {temperature}")
+def afficher_temperature(temperature, temperature_moyenne):
+    print(f"Test affichage: Cible {temperature} | Moyenne {temperature_moyenne}")
     try:
-        temp_entiere = int(temperature)
-        texte_ecran = f"{temp_entiere}{temperature_moyenne}" 
+        temp1 = int(temperature)
+        temp2 = int(temperature_moyenne)
+        texte_ecran = f"{temp1:02d}{temp2:02d}" 
+        
         display.show(texte_ecran)
         
     except Exception as e:
         print(f"Erreur d'affichage : {e}")
-
-
